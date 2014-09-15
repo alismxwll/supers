770a527fb483e04106244d5093421680175cda07
Andrew Akers <student@epicodus.com>
2014-09-15 12:00:38 -0700

    create posts

---
+++ app/controllers/Posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all.order('created_at DESC').first(10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      redirect_to root_path, notice: "Post Added"
+    else
+      render 'index'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path, notice: "Post Updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, "Post Removed"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
